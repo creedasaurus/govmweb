94787f488c88f2c8d35678a68e882a03d4688931
Creed Haymond <creedasaurus@gmail.com>
2017-10-31 14:23:43 -0600

    Setting up build

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 
+console.log('Hello Webpack React stuff!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  entry: __dirname + '/src/index.jsx',
 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/binary-code.png',
+    }),
+  ],
+
+  devtool: 'source-map',
+
+  devServer: {
+    publicPath: path.join('/dist/'),
+  },
 };
